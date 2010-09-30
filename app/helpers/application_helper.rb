c8e1ef6477ebd688946a5ee82d680bb4c4de846e
Gautham Nookala <gnook86@gmail.com>
2010-09-30 15:59:24 -0700

    customized for isb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
   # Return a title on a per-page basis.
+  def logo
+    logo = image_tag("logo.jpg", :alt => "Sample App", :class => "round")
+	link_to logo, root_path
+  end
+  
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Wholesale Management System"
     if @title.nil?
       base_title
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
-	match '/contact', :to => 'pages#contact'
-	match '/about',   :to => 'pages#about'
-	match '/help',    :to => 'pages#help'
+  get "users/new"
 
-	root :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
+
 end
